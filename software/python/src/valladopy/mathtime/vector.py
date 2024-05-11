ef46b16c87716818960c491e517ee985c7d5b946
samira <samotiwala@gmail.com>
2024-05-11 16:19:36 -0700

    add function to calculate vec between two angles + unit test

--- software/python/src/valladopy/mathtime/vector.py
+++ software/python/src/valladopy/mathtime/vector.py
@@ -8,6 +8,8 @@
 
 import numpy as np
 
+from ..constants import SMALL
+
 
 ###############################################################################
 # Axes Rotations
@@ -74,3 +76,36 @@ def rot3(vec, xval):
         ]
     )
     return outvec
+
+
+###############################################################################
+# Vector Math
+###############################################################################
+
+
+def angle(v1, v2):
+    """Calculates the angle between two vectors in radians
+
+    This function computes the angle between two vectors using the dot product
+    and the magnitudes of the vectors. The function handles cases where the dot
+    product might slightly exceed the interval [-1, 1] due to numerical
+    precision issues by clipping the value. If either vector has zero magnitude,
+    the function returns `np.nan`to indicate that the angle is not computable.
+
+    Args:
+        v1 (numpy.ndarray): The first vector.
+        v2 (numpy.ndarray): The second vector.
+
+    Returns:
+        float: The angle between the two vectors in radians. Returns `np.nan` if
+               either vector is zero.
+    """
+    mag_v1 = np.linalg.norm(v1)
+    mag_v2 = np.linalg.norm(v2)
+
+    if mag_v1 * mag_v2 > SMALL ** 2:
+        cos_angle = np.dot(v1, v2) / (mag_v1 * mag_v2)
+        cos_angle = np.clip(cos_angle, -1, 1)  # Keep cosine within domain
+        return np.arccos(cos_angle)
+    else:
+        return np.nan
--- software/python/tests/mathtime/test_vector.py
+++ software/python/tests/mathtime/test_vector.py
@@ -14,3 +14,20 @@ import src.valladopy.mathtime.vector as vec
 ])
 def test_rotations(func, angle, vector, expected):
     assert np.allclose(func(vector, angle), expected, rtol=1e-12)
+
+
+@pytest.mark.parametrize('v1, v2, expected_angle', [
+    (np.array([1, 0, 0]), np.array([1, 0, 0]), 0),
+    (np.array([1, 0, 0]), np.array([-1, 0, 0]), np.pi),
+    (np.array([1, 0, 0]), np.array([0, 1, 0]), np.pi / 2),
+    (np.array([1, 0, 0]), np.array([1, 1, 0]), np.arccos(1 / np.sqrt(2))),
+    (np.array([0, 0, 0]), np.array([1, 0, 0]), np.nan),
+    (np.array([1, 0, 0]), np.array([0, 0, 0]), np.nan),
+    (np.array([1e-10, 0, 0]), np.array([1e-10, 0, 0]), np.nan),
+])
+def test_angle_between_vectors(v1, v2, expected_angle):
+    angle = vec.angle(v1, v2)
+    assert (
+        np.isnan(angle) if np.isnan(expected_angle)
+        else abs(angle - expected_angle) < 1e-12
+    )
