ae6e2cae280b8abdc39c5ead1a66f6a7ee09d89f
samira <samotiwala@gmail.com>
2024-11-08 17:23:43 -0800

    black updates

--- software/python/tests/astro/twobody/test_utils.py
+++ software/python/tests/astro/twobody/test_utils.py
@@ -122,25 +122,25 @@ def test_checkhitearth(
 
 class TestHills:
     alt_tgt = 590  # target satellite altitude
-    dts = 300      # TOF
+    dts = 300  # TOF
 
     @pytest.mark.parametrize(
-        'r, v, rint_exp, vint_exp',
+        "r, v, rint_exp, vint_exp",
         [
             (
                 [500 + const.RE, 0, 0],
                 [0, np.sqrt(const.MU / (500 + const.RE)), 0],
                 [8699.5464562552, 1886.98432911071, 0.0],
-                [12.035252716871558, 3.658653928010125, 0.0]
+                [12.035252716871558, 3.658653928010125, 0.0],
             ),
             (
                 # Vallado 2007, Example 6-14
                 [0, 0, 0],
                 [-0.1, -0.04, -0.02],
                 [-33.345724432187964, -1.473560307487475, -5.894530311607677],
-                [-0.120337093866688, 0.032387584447866, -0.018949031717993]
-            )
-        ]
+                [-0.120337093866688, 0.032387584447866, -0.018949031717993],
+            ),
+        ],
     )
     def test_hillsr(self, r, v, rint_exp, vint_exp):
         rint, vint = utils.hillsr(r, v, self.alt_tgt, self.dts)
