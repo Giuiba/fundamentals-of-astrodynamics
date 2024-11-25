3e83f4a58f5f1aa09285e243167bc51e7d23aa0d
samira <samotiwala@gmail.com>
2024-11-24 20:36:07 -0800

    add fk4i functions & clean up

--- software/python/src/valladopy/astro/time/fk4.py
+++ software/python/src/valladopy/astro/time/fk4.py
@@ -10,8 +10,13 @@
 import numpy as np
 
 
+###############################################################################
+# B1950 to J2000 conversion matrices
+###############################################################################
+
+
 def fk4() -> np.ndarray:
-    """Returns the B1950 to J2000 conversion matrix (in-book approach).
+    """Returns the B1950 to J2000 conversion matrix (book approach).
 
     Returns:
         np.ndarray: B1950 to J2000 conversion matrix
@@ -50,8 +55,8 @@ def fk4_stk() -> np.ndarray:
     )
 
 
-def fk4_extended() -> np.ndarray:
-    """Returns the B1950 to J2000 conversion matrix (6x6 extended version).
+def fk4_exp_supp_6x6() -> np.ndarray:
+    """Returns the B1950 to J2000 conversion matrix (almanac supplement, 6x6).
 
     Returns:
         np.ndarray: B1950 to J2000 conversion matrix
@@ -88,5 +93,91 @@ def fk4_extended() -> np.ndarray:
             [-0.000551, -0.238565, 0.435739, 0.99994704, -0.01118251, -0.00485767],
             [0.238514, -0.002667, -0.008541, 0.01118251, 0.99995883, -0.00002718],
             [-0.435623, 0.012254, 0.002117, 0.00485767, -0.00002714, 1.00000956],
+        ]
+    )
+
+
+###############################################################################
+# J2000 to B1950 conversion matrices
+###############################################################################
+
+
+def fk4i() -> np.ndarray:
+    """Returns the J2000 to B1950 conversion matrix (book approach).
+
+    Returns:
+        np.ndarray: J2000 to B1950 conversion matrix
+    """
+    return np.array(
+        [
+            [0.9999256794956877, 0.0111814832391717, 0.0048590037723143],
+            [-0.0111814832204662, 0.9999374848933135, -0.0000271702937440],
+            [-0.0048590038153592, -0.0000271625947142, 0.9999881946043742],
+        ]
+    )
+
+
+def fk4i_stk() -> np.ndarray:
+    """Returns the J2000 to B1950 conversion matrix (STK approach)."""
+    return np.array(
+        [
+            [0.999925678612394, 0.011181874524964, 0.004858284884778],
+            [-0.011181874556714, 0.999937480517880, -0.000027156932874],
+            [-0.004858284812600, -0.000027169816135, 0.999988198095508],
+        ]
+    )
+
+
+def fk4i_exp_supp_3x3() -> np.ndarray:
+    """Returns the J2000 to B1950 FK4 conversion matrix
+    (almanac supplement, 3x3).
+
+    Returns:
+        np.ndarray: J2000 to B1950 conversion matrix
+    """
+    return np.array(
+        [
+            [0.9999256795, 0.0111814828, 0.0048590039],
+            [-0.0111814828, 0.9999374849, -0.0000271771],
+            [-0.0048590040, -0.0000271557, 0.9999881946],
+        ]
+    )
+
+
+def fk4i_exp_supp_6x6() -> np.ndarray:
+    """Returns the J2000 to B1950 FK4 conversion matrix (almanac supplement, 6x6).
+
+    Returns:
+        np.ndarray: J2000 to B1950 conversion matrix
+    """
+    return np.array(
+        [
+            [
+                0.9999256795,
+                0.0111814828,
+                0.0048590039,
+                -0.00000242389840,
+                -0.00000002710544,
+                -0.00000001177742,
+            ],
+            [
+                -0.0111814828,
+                0.9999374849,
+                -0.0000271771,
+                0.00000002710544,
+                -0.00000242392702,
+                0.00000000006585,
+            ],
+            [
+                -0.0048590040,
+                -0.0000271557,
+                0.9999881946,
+                0.00000001177742,
+                0.00000000006585,
+                -0.00000242404995,
+            ],
+            [-0.000551, 0.238509, -0.435614, 0.99990432, 0.01118145, 0.00485852],
+            [-0.238560, -0.002667, 0.012254, -0.01118145, 0.99991613, -0.00002717],
+            [0.435730, -0.008541, 0.002117, -0.00485852, -0.00002716, 0.99996684],
         ]
     )
