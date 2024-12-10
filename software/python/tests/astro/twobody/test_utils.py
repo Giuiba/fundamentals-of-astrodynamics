5978763b1f5799eb9f7cbf64a79232d8b7b9d220
samira <samotiwala@gmail.com>
2024-12-09 22:17:33 -0800

    move hills eqs + tests

--- software/python/tests/astro/twobody/test_utils.py
+++ software/python/tests/astro/twobody/test_utils.py
@@ -2,8 +2,7 @@ import numpy as np
 import pytest
 
 import src.valladopy.astro.twobody.utils as utils
-import src.valladopy.constants as const
-from ...conftest import DEFAULT_TOL, custom_allclose
+from ...conftest import DEFAULT_TOL
 
 
 @pytest.mark.parametrize(
@@ -136,37 +135,3 @@ def test_checkhitearth(
     hitearth, hitearthstr = utils.checkhitearth(altpad, r1, v1, r2, v2, nrev)
     assert hitearth == expected_hitearth
     assert hitearthstr == expected_hitearthstr
-
-
-class TestHills:
-    alt_tgt = 590  # target satellite altitude
-    dts = 300  # TOF
-
-    @pytest.mark.parametrize(
-        "r, v, rint_exp, vint_exp",
-        [
-            (
-                [500 + const.RE, 0, 0],
-                [0, np.sqrt(const.MU / (500 + const.RE)), 0],
-                [8699.5464562552, 1886.98432911071, 0.0],
-                [12.035252716871558, 3.658653928010125, 0.0],
-            ),
-            (
-                # Vallado 2007, Example 6-14
-                [0, 0, 0],
-                [-0.1, -0.04, -0.02],
-                [-33.345724432187964, -1.473560307487475, -5.894530311607677],
-                [-0.120337093866688, 0.032387584447866, -0.018949031717993],
-            ),
-        ],
-    )
-    def test_hillsr(self, r, v, rint_exp, vint_exp):
-        rint, vint = utils.hillsr(r, v, self.alt_tgt, self.dts)
-        assert np.allclose(rint, rint_exp, rtol=DEFAULT_TOL)
-        assert np.allclose(vint, vint_exp, rtol=DEFAULT_TOL)
-
-    def test_hillsv(self):
-        r = [-70.933, 20.357, -11.17]
-        vint_exp = [0.274190838333, 0.013466718841345, 0.035907981315]
-        vint = utils.hillsv(r, self.alt_tgt, self.dts)
-        assert custom_allclose(vint, vint_exp)
