0ffea06065d8cd7b2b406c5e10f0466692ee5585
samira <samotiwala@gmail.com>
2024-12-10 11:02:25 -0800

    add sight function and test

---
+++ software/python/tests/astro/celestial/test_utils.py
@@ -0,0 +1,43 @@
+import logging
+import pytest
+
+import src.valladopy.astro.celestial.utils as utils
+
+
+@pytest.mark.parametrize(
+    "r2, earth_model, los, tmin",
+    [
+        (
+            [0.0, 5740.323, 3189.068],
+            utils.EarthModel.ELLIPSOIDAL,
+            False,
+            0.508224865077047,
+        ),
+        (
+            [0.0, 5740.323, 3189.068],
+            utils.EarthModel.SPHERICAL,
+            False,
+            0.5082352992389487,
+        ),
+        (
+            [122233179.72368076, -76150708.25425531, -33016373.913704105],
+            utils.EarthModel.ELLIPSOIDAL,
+            True,
+            -2.3342657116393083e-05,
+        ),
+        (
+            [122233179.72368076, -76150708.25425531, -33016373.913704105],
+            utils.EarthModel.SPHERICAL,
+            True,
+            -2.3290642130715177e-05,
+        ),
+    ],
+)
+def test_sight(r2, earth_model, los, tmin, caplog):
+    # Vallado 2022, Example 5-6
+    r1 = [0.0, -4464.696, -5102.509]
+
+    # Call function with logging
+    with caplog.at_level(logging.DEBUG):
+        assert utils.sight(r1, r2, earth_model) == los
+        assert f"Minimum parametric value (tmin): {tmin}" in caplog.messages[0]
