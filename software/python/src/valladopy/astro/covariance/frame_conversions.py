3ca111ad235ecc54571ed033a215726944bbdc05
samira <samotiwala@gmail.com>
2024-12-11 14:27:40 -0800

    ad first cut of covct2cl + test

---
+++ software/python/src/valladopy/astro/covariance/frame_conversions.py
@@ -0,0 +1,284 @@
+import numpy as np
+
+from ..twobody import frame_conversions as fc
+from ...constants import KM2M, MUM
+
+
+########################################################################################
+# Classical Elements
+########################################################################################
+
+
+def covct2cl(
+    cartcov: np.ndarray, cartstate: np.ndarray, use_mean_anom: bool = False
+) -> tuple[np.ndarray, np.ndarray]:
+    """Transforms a 6x6 covariance matrix from Cartesian elements to classical orbital
+    elements.
+
+    Args:
+        cartcov: 6x6 Cartesian covariance matrix.
+        cartstate: 6x1 Cartesian orbit state [rx, ry, rz, vx, vy, vz].
+        use_mean_anom: Flag to use mean anomaly instead of true anomaly
+
+    Returns:
+        classcov: 6x6 classical covariance matrix.
+        tm: Transformation matrix.
+    """
+    # Parse the input state vector
+    rx, ry, rz, vx, vy, vz = cartstate * KM2M  # convert to meters
+    reci_m = np.array([rx, ry, rz])
+    veci_m = np.array([vx, vy, vz])
+    reci = reci_m / KM2M  # convert back to km
+    veci = veci_m / KM2M
+
+    # Convert to classical orbital elements
+    _, a, ecc, incl, omega, argp, nu, *_ = fc.rv2coe(reci, veci)
+    a *= KM2M
+    n = np.sqrt(MUM / a**3)
+
+    # Common quantities
+    sqrt1me2 = np.sqrt(1.0 - ecc**2)
+    magr = np.linalg.norm(reci_m)
+    magr3 = magr**3
+    magv = np.linalg.norm(veci_m)
+
+    # Eccentricity vector
+    r_dot_v = np.dot(reci_m, veci_m)
+    ecc_term = magv**2 - MUM / magr
+    ecc_vec = (ecc_term * reci_m - r_dot_v * veci_m) / MUM
+
+    # Node vector
+    h_vec = np.cross(reci_m, veci_m)
+    h = np.linalg.norm(h_vec)
+    node_vec = np.cross([0.0, 0.0, 1.0], h_vec)
+    node = np.linalg.norm(node_vec)
+
+    # Additional terms for argument of periapsis and true anomaly
+    n_dot_e = np.dot(node_vec, ecc_vec)
+    sign_w = np.sign((magv**2 - MUM / magr) * rz - r_dot_v * vz)
+    cos_w = n_dot_e / (ecc * node)
+    w_scale = -sign_w / np.sqrt(1 - cos_w**2)
+
+    # Additional terms for true anomaly
+    r_dot_e = np.dot(reci_m, ecc_vec)
+    cos_nu = r_dot_e / (magr * ecc)
+    sign_nu = np.sign(r_dot_v)
+    nu_scale = -sign_nu / np.sqrt(1 - cos_nu**2)
+
+    # Compute partial derivatives
+    tm = np.zeros((6, 6))
+
+    # Partials of a w.r.t. (rx, ry, rz, vx, vy, vz)
+    p0 = 2.0 * a**2 / magr**3
+    p1 = 2.0 / (n**2 * a)
+    tm[0, :3] = p0 * reci_m
+    tm[0, 3:] = p1 * veci_m
+
+    # Partials of ecc w.r.t. (rx, ry, rz, vx, vy, vz)
+    p0 = 1.0 / (MUM * ecc)
+    tm[1, 0] = -p0 * (
+        ((vx * vy - MUM * rx * ry / magr3) * ecc_vec[1])
+        + ((vx * vz - MUM * rx * rz / magr3) * ecc_vec[2])
+        - (vy**2 + vz**2 - MUM / magr + MUM * rx**2 / magr3) * ecc_vec[0]
+    )
+    tm[1, 1] = -p0 * (
+        ((vx * vy - MUM * rx * ry / magr3) * ecc_vec[0])
+        + ((vy * vz - MUM * ry * rz / magr3) * ecc_vec[2])
+        - (vx**2 + vz**2 - MUM / magr + MUM * ry**2 / magr3) * ecc_vec[1]
+    )
+    tm[1, 2] = -p0 * (
+        ((vx * vz - MUM * rx * rz / magr3) * ecc_vec[0])
+        + ((vy * vz - MUM * ry * rz / magr3) * ecc_vec[1])
+        - (vx**2 + vy**2 - MUM / magr + MUM * rz**2 / magr3) * ecc_vec[2]
+    )
+    tm[1, 3] = -p0 * (
+        ((rx * vy - 2 * ry * vx) * ecc_vec[1])
+        + ((ry * vy + rz * vz) * ecc_vec[0])
+        + ((rx * vz - 2 * rz * vx) * ecc_vec[2])
+    )
+    tm[1, 4] = -p0 * (
+        ((ry * vx - 2 * rx * vy) * ecc_vec[0])
+        + ((rx * vx + rz * vz) * ecc_vec[1])
+        + ((ry * vz - 2 * rz * vy) * ecc_vec[2])
+    )
+    tm[1, 5] = -p0 * (
+        ((rx * vx + ry * vy) * ecc_vec[2])
+        + ((rz * vx - 2 * rx * vz) * ecc_vec[0])
+        + ((rz * vy - 2 * ry * vz) * ecc_vec[1])
+    )
+
+    # Partials of inclination w.r.t. (rx, ry, rz, vx, vy, vz)
+    p3 = 1.0 / node
+    tm[2, 0] = -p3 * (vy - h_vec[2] * (vy * h_vec[2] - vz * h_vec[1]) / h**2)
+    tm[2, 1] = p3 * (vx - h_vec[2] * (vx * h_vec[2] - vz * h_vec[0]) / h**2)
+    tm[2, 2] = -p3 * (h_vec[2] * (vy * h_vec[0] - vx * h_vec[1]) / h**2)
+    tm[2, 3] = p3 * (ry - h_vec[2] * (ry * h_vec[2] - rz * h_vec[1]) / h**2)
+    tm[2, 4] = -p3 * (rx - h_vec[2] * (rx * h_vec[2] - rz * h_vec[0]) / h**2)
+    tm[2, 5] = p3 * (h_vec[2] * (ry * h_vec[0] - rx * h_vec[1]) / h**2)
+
+    # Partials of node w.r.t. (rx, ry, rz, vx, vy, vz)
+    p4 = 1.0 / (node**2)
+    tm[3, 0] = -p4 * vz * node_vec[1]
+    tm[3, 1] = p4 * vz * node_vec[0]
+    tm[3, 2] = p4 * (vx * node_vec[1] - vy * node_vec[0])
+    tm[3, 3] = p4 * rz * node_vec[1]
+    tm[3, 4] = -p4 * rz * node_vec[0]
+    tm[3, 5] = p4 * (ry * node_vec[0] - rx * node_vec[1])
+
+    # Partials of argp w.r.t. (rx, ry, rz, vx, vy, vz)
+    tm[4, 0] = (
+        -h_vec[1] * (vy**2 + vz**2 - MUM / magr + MUM * rx**2 / magr3)
+        - h_vec[0] * (vx * vy - MUM * rx * ry / magr3)
+        + vz * MUM * ecc_vec[0]
+    )
+    tm[4, 0] = (
+        tm[4, 0] / (MUM * node * ecc)
+        + vz * h_vec[1] * n_dot_e / (node**3 * ecc)
+        - tm[1, 0] * n_dot_e / (node * ecc**2)
+    ) * w_scale
+
+    tm[4, 1] = (
+        h_vec[0] * (vx**2 + vz**2 - MUM / magr + MUM * ry**2 / magr3)
+        + h_vec[1] * (vx * vy - MUM * rx * ry / magr3)
+        + vz * MUM * ecc_vec[1]
+    )
+    tm[4, 1] = (
+        tm[4, 1] / (MUM * node * ecc)
+        - vz * h_vec[0] * n_dot_e / (node**3 * ecc)
+        - tm[1, 1] * n_dot_e / (node * ecc**2)
+    ) * w_scale
+
+    tm[4, 2] = (
+        -h_vec[1] * (vx * vz - MUM * rx * rz / magr3)
+        + h_vec[0] * (vy * vz - MUM * ry * rz / magr3)
+        + vx * MUM * ecc_vec[0]
+        + vy * MUM * ecc_vec[1]
+    )
+    tm[4, 2] = (
+        -tm[4, 2] / (MUM * node * ecc)
+        + (vy * h_vec[0] - vx * h_vec[1]) * n_dot_e / (node**3 * ecc)
+        - tm[1, 2] * n_dot_e / (node * ecc**2)
+    ) * w_scale
+
+    tm[4, 3] = (
+        (rx * vy - 2 * ry * vx) * h_vec[0]
+        - h_vec[1] * (ry * vy + rz * vz)
+        + rz * MUM * ecc_vec[0]
+    )
+    tm[4, 3] = (
+        -tm[4, 3] / (MUM * node * ecc)
+        - rz * h_vec[1] * n_dot_e / (node**3 * ecc)
+        - tm[1, 3] * n_dot_e / (node * ecc**2)
+    ) * w_scale
+
+    tm[4, 4] = (
+        -(ry * vx - 2 * rx * vy) * h_vec[1]
+        + h_vec[0] * (rx * vx + rz * vz)
+        + rz * MUM * ecc_vec[1]
+    )
+    tm[4, 4] = (
+        -tm[4, 4] / (MUM * node * ecc)
+        + rz * h_vec[0] * n_dot_e / (node**3 * ecc)
+        - tm[1, 4] * n_dot_e / (node * ecc**2)
+    ) * w_scale
+
+    tm[4, 5] = (
+        -(rz * vx - 2 * rx * vz) * h_vec[1]
+        + h_vec[0] * (rz * vy - 2 * ry * vz)
+        - rx * MUM * ecc_vec[0]
+        - ry * MUM * ecc_vec[1]
+    )
+    tm[4, 5] = (
+        -tm[4, 5] / (MUM * node * ecc)
+        + (rx * h_vec[1] - ry * h_vec[0]) * n_dot_e / (node**3 * ecc)
+        - tm[1, 5] * n_dot_e / (node * ecc**2)
+    ) * w_scale
+
+    # Partials of nu/M w.r.t. (rx, ry, rz, vx, vy, vz)
+    tm[5, 0] = (
+        ry * (vx * vy - MUM * rx * ry / magr3)
+        - rx * ecc_term
+        + rz * (vx * vz - MUM * rx * rz / magr3)
+        - rx * (vy**2 + vz**2 - MUM / magr + MUM * rx**2 / magr3)
+        + vx * r_dot_v
+    )
+    tm[5, 0] = (
+        -tm[5, 0] / (MUM * magr * ecc)
+        - rx * r_dot_e / (magr3 * ecc)
+        - tm[1, 0] * r_dot_e / (magr * ecc**2)
+    ) * nu_scale
+
+    tm[5, 1] = (
+        rx * (vx * vy - MUM * rx * ry / magr3)
+        - ry * ecc_term
+        + rz * (vy * vz - MUM * ry * rz / magr3)
+        - ry * (vx**2 + vz**2 - MUM / magr + MUM * ry**2 / magr3)
+        + vy * r_dot_v
+    )
+    tm[5, 1] = (
+        -tm[5, 1] / (MUM * magr * ecc)
+        - ry * r_dot_e / (magr3 * ecc)
+        - tm[1, 1] * r_dot_e / (magr * ecc**2)
+    ) * nu_scale
+
+    tm[5, 2] = (
+        rx * (vx * vz - MUM * rx * rz / magr3)
+        - rz * ecc_term
+        + ry * (vy * vz - MUM * ry * rz / magr3)
+        - rz * (vx**2 + vy**2 - MUM / magr + MUM * rz**2 / magr3)
+        + vz * r_dot_v
+    )
+    tm[5, 2] = (
+        -tm[5, 2] / (MUM * magr * ecc)
+        - rz * r_dot_e / (magr3 * ecc)
+        - tm[1, 2] * r_dot_e / (magr * ecc**2)
+    ) * nu_scale
+
+    tm[5, 3] = (
+        ry * (rx * vy - 2 * ry * vx)
+        + rx * (ry * vy + rz * vz)
+        + rz * (rx * vz - 2 * rz * vx)
+    )
+    tm[5, 3] = (
+        -tm[5, 3] / (MUM * magr * ecc) - tm[1, 3] * r_dot_e / (magr * ecc**2)
+    ) * nu_scale
+
+    tm[5, 4] = (
+        rx * (ry * vx - 2 * rx * vy)
+        + ry * (rx * vx + rz * vz)
+        + rz * (ry * vz - 2 * rz * vy)
+    )
+    tm[5, 4] = (
+        -tm[5, 4] / (MUM * magr * ecc) - tm[1, 4] * r_dot_e / (magr * ecc**2)
+    ) * nu_scale
+
+    tm[5, 5] = (
+        rz * (rx * vx + ry * vy)
+        + rx * (rz * vx - 2 * rx * vz)
+        + ry * (rz * vy - 2 * ry * vz)
+    )
+    tm[5, 5] = (
+        -tm[5, 5] / (MUM * magr * ecc) - tm[1, 5] * r_dot_e / (magr * ecc**2)
+    ) * nu_scale
+
+    # Update partials for mean anomaly if specified
+    if use_mean_anom:
+        dmdnu = (sqrt1me2**2) ** 1.5 / (1.0 + ecc * np.cos(nu)) ** 2
+        dmde = (
+            -np.sin(nu)
+            * (
+                (ecc * np.cos(nu) + 1)
+                * (ecc + np.cos(nu))
+                / np.sqrt((ecc + np.cos(nu)) ** 2)
+                + 1.0
+                - 2.0 * ecc**2
+                - ecc**3 * np.cos(nu)
+            )
+            / ((ecc * np.cos(nu) + 1.0) ** 2 * sqrt1me2)
+        )
+        tm[5, :] = tm[5, :] * dmdnu + tm[1, :] * dmde
+
+    # Calculate the classical covariance matrix
+    classcov = tm @ cartcov @ tm.T
+
+    return classcov, tm
