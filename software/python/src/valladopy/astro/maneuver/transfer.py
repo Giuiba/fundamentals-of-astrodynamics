82cc1f261dea654c5cd05e4095366d00efdce607
samira <samotiwala@gmail.com>
2024-12-12 12:30:09 -0800

    add onetangent transfer function + tests

--- software/python/src/valladopy/astro/maneuver/transfer.py
+++ software/python/src/valladopy/astro/maneuver/transfer.py
@@ -129,3 +129,91 @@ def bielliptic(
         )
 
     return deltava, deltavb, deltavc, dttu
+
+
+def onetangent(
+    rinit: float,
+    rfinal: float,
+    efinal: float,
+    nuinit: float,
+    nutran: float,
+    tol: float = 1e-6,
+) -> tuple[float, float, float, float, float, float, float]:
+    """Calculates the delta-v values for a one tangent transfer, either circle-to-circle
+    or ellipse-to-ellipse.
+
+    References:
+        Vallado 2007, p. 335, Algorithm 38
+
+    Args:
+        rinit (float): Initial position magnitude in km
+        rfinal (float): Final position magnitude in km
+        efinal (float): Eccentricity of the final orbit
+        nuinit (float): True anomaly of the initial orbit in radians (0 or pi)
+        nutran (float): True anomaly of the transfer orbit in radians
+                        (same quadrant as `nuinit`)
+        tol (float): Tolerance for checking if transfer orbit is valid (default 1e-6)
+
+    Returns:
+        tuple: (deltava, deltavb, dttu, etran, atran, vtrana, vtranb)
+            deltava (float): Change in velocity at point A in km/s
+            deltavb (float): Change in velocity at point B in km/s
+            dttu (float): Time of flight for the transfer in seconds
+            etran (float): Eccentricity of the transfer orbit
+            atran (float): Semi-major axis of the transfer orbit in km
+            vtrana (float): Velocity of the transfer orbit at point A in km/s
+            vtranb (float): Velocity of the transfer orbit at point B in km/s
+
+    Raises:
+        ValueError: If the one-tangent burn is not possible for the given inputs
+    """
+    # Initialize variables
+    dttu = 0.0
+
+    # Ratio of initial to final orbit radii
+    ratio = rinit / rfinal
+
+    # Determine eccentricity of transfer orbit
+    if abs(nuinit) < 0.01:  # near 0 or 180 degrees
+        etran = (ratio - 1.0) / (np.cos(nutran) - ratio)
+    else:
+        etran = (ratio - 1.0) / (np.cos(nutran) + ratio)
+
+    # Check if transfer orbit is valid
+    if etran >= 0.0:
+        # Semi-major axes of initial, final, and transfer orbits
+        afinal = (rfinal * (1.0 + efinal * np.cos(nutran))) / (1.0 - efinal**2)
+
+        if abs(etran - 1.0) > tol:
+            atran = (rinit * (1.0 + etran * np.cos(nuinit))) / (1.0 - etran**2)
+        else:
+            atran = np.inf  # parabolic orbit (infinite semi-major axis)
+
+        # Calculate velocities
+        vinit = np.sqrt(const.MU / rinit)
+        vtrana = np.sqrt((2.0 * const.MU) / rinit - (const.MU / atran))
+        deltava = np.abs(vtrana - vinit)
+
+        vfinal = np.sqrt((2.0 * const.MU) / rfinal - (const.MU / afinal))
+        vtranb = np.sqrt((2.0 * const.MU) / rfinal - (const.MU / atran))
+        fpatranb = np.arctan2(etran * np.sin(nutran), 1.0 + etran * np.cos(nutran))
+        fpafinal = np.arctan2(efinal * np.sin(nutran), 1.0 + efinal * np.cos(nutran))
+        deltavb = np.sqrt(
+            vtranb**2 + vfinal**2 - 2.0 * vtranb * vfinal * np.cos(fpatranb - fpafinal)
+        )
+
+        # Calculate time of flight
+        if etran < 1.0:
+            sinv = (np.sqrt(1.0 - etran**2) * np.sin(nutran)) / (
+                1.0 + etran * np.cos(nutran)
+            )
+            cosv = (etran + np.cos(nutran)) / (1.0 + etran * np.cos(nutran))
+            e = np.arctan2(sinv, cosv)
+            eainit = 0.0 if abs(nuinit) < 0.01 else np.pi
+            dttu = np.sqrt(atran**3 / const.MU) * (
+                e - etran * np.sin(e) - (eainit - etran * np.sin(eainit))
+            )
+    else:
+        raise ValueError("The one-tangent burn is not possible for this case.")
+
+    return deltava, deltavb, dttu, etran, atran, vtrana, vtranb
--- software/python/tests/astro/maneuver/test_transfer.py
+++ software/python/tests/astro/maneuver/test_transfer.py
@@ -7,6 +7,18 @@ import src.valladopy.constants as const
 from ...conftest import custom_isclose
 
 
+@pytest.fixture
+def rinit_rfinal():
+    rinit = const.RE + 191.3411
+    rfinal = const.RE + 35781.34857
+    return rinit, rfinal
+
+
+@pytest.fixture
+def nuinit_nufinal():
+    return 0.0, np.pi
+
+
 @pytest.mark.parametrize(
     "einit, efinal, deltava_expected, deltavb_expected",
     [
@@ -16,15 +28,12 @@ from ...conftest import custom_isclose
         (0.00123, 0.024, 2.4522496326398677, 1.4410649593617622),
     ],
 )
-def test_hohmann(einit, efinal, deltava_expected, deltavb_expected):
-    # Input values
-    rinit = const.RE + 191.3411
-    rfinal = const.RE + 35781.34857
-    nuinit, nufinal = 0.0, np.pi
-
+def test_hohmann(
+    rinit_rfinal, nuinit_nufinal, einit, efinal, deltava_expected, deltavb_expected
+):
     # Calculate Hohmann transfer
     deltava, deltavb, dttu = transfer.hohmann(
-        rinit, rfinal, einit, efinal, nuinit, nufinal
+        *rinit_rfinal, einit, efinal, *nuinit_nufinal
     )
 
     # Expected results
@@ -43,17 +52,22 @@ def test_hohmann(einit, efinal, deltava_expected, deltavb_expected):
     ],
 )
 def test_bielliptic(
-    einit, efinal, deltava_expected, deltavb_expected, deltavc_expected
+    rinit_rfinal,
+    nuinit_nufinal,
+    einit,
+    efinal,
+    deltava_expected,
+    deltavb_expected,
+    deltavc_expected,
 ):
     # Input values
-    rinit = const.RE + 191.3411
+    rinit, _ = rinit_rfinal
     rb = const.RE + 503873.0
     rfinal = const.RE + 376310.0
-    nuinit, nufinal = 0.0, np.pi
 
     # Calculate bielliptic transfer
     deltava, deltavb, deltavc, dttu = transfer.bielliptic(
-        rinit, rb, rfinal, einit, efinal, nuinit, nufinal
+        rinit, rb, rfinal, einit, efinal, *nuinit_nufinal
     )
 
     # Expected results
@@ -61,3 +75,43 @@ def test_bielliptic(
     assert custom_isclose(deltavb, deltavb_expected)
     assert custom_isclose(deltavc, deltavc_expected)
     assert custom_isclose(dttu / const.HR2SEC, 593.9199987732167)
+
+
+@pytest.mark.parametrize(
+    "efinal, deltavb_expected",
+    [
+        # Vallado 2007, Example 6-3
+        (0.0, 2.1239372795577847),
+        # Test non-zero eccentricity
+        (0.024, 2.0814417965636647),
+    ],
+)
+def test_onetangent(rinit_rfinal, nuinit_nufinal, efinal, deltavb_expected):
+    # Input values
+    nuinit, _ = nuinit_nufinal
+    nutran = np.radians(160)
+
+    # Calculate one-tangent transfer
+    deltava, deltavb, dttu, etran, atran, vtrana, vtranb = transfer.onetangent(
+        *rinit_rfinal, efinal, nuinit, nutran
+    )
+
+    # Expected results
+    assert custom_isclose(deltava, 2.575396079597792)
+    assert custom_isclose(deltavb, deltavb_expected)
+    assert custom_isclose(dttu / const.MIN2SEC, 207.44540183723626)
+    assert custom_isclose(etran, 0.7705727464030682)
+    assert custom_isclose(atran, 28634.25027761329)
+    assert custom_isclose(vtrana, 10.364786573669267)
+    assert custom_isclose(vtranb, 2.2335537984044187)
+
+
+def test_onetangent_bad_transfer(rinit_rfinal, nuinit_nufinal):
+    # Input values
+    efinal = 0.0
+    nuinit, _ = nuinit_nufinal
+    nutran = np.radians(0)
+
+    # Calculate one-tangent transfer
+    with pytest.raises(ValueError):
+        transfer.onetangent(*rinit_rfinal, efinal, nuinit, nutran)
