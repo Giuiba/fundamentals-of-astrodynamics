c1d8e9fbd18c646b0bef0f8f7687d4a77914bd32
samira <samotiwala@gmail.com>
2024-12-12 10:52:54 -0800

    add bielliptic transfer function + tests

--- software/python/src/valladopy/astro/maneuver/transfer.py
+++ software/python/src/valladopy/astro/maneuver/transfer.py
@@ -25,7 +25,7 @@ def hohmann(
     or ellipse-to-ellipse.
 
     References:
-        Vallado 2007, pp. 327, Algorithm 36
+        Vallado 2007, p. 327, Algorithm 36
 
     Args:
         rinit (float): Initial position magnitude in km
@@ -37,9 +37,9 @@ def hohmann(
 
     Returns:
         tuple: (deltava, deltavb, dttu)
-            - deltava (float): Change in velocity at point A in km/s
-            - deltavb (float): Change in velocity at point B in km/s
-            - dttu (float): Time of flight for the transfer in seconds
+            deltava (float): Change in velocity at point A in km/s
+            deltavb (float): Change in velocity at point B in km/s
+            dttu (float): Time of flight for the transfer in seconds
     """
     # Semi-major axes of initial, transfer, and final orbits
     ainit = (rinit * (1.0 + einit * np.cos(nuinit))) / (1.0 - einit**2)
@@ -64,3 +64,68 @@ def hohmann(
         dttu = np.pi * np.sqrt(atran**3 / const.MU)
 
     return deltava, deltavb, dttu
+
+
+def bielliptic(
+    rinit: float,
+    rb: float,
+    rfinal: float,
+    einit: float,
+    efinal: float,
+    nuinit: float,
+    nufinal: float,
+) -> tuple[float, float, float, float]:
+    """Calculates the delta-v values for a bi-elliptic transfer, either circle-to-circle
+    or ellipse-to-ellipse.
+
+    References:
+        Vallado 2007, pp. 327, Algorithm 37
+
+    Args:
+        rinit (float): Initial position magnitude in km
+        rb (float): Intermediate position magnitude in km
+        rfinal (float): Final position magnitude in km
+        einit (float): Eccentricity of the initial orbit
+        efinal (float): Eccentricity of the final orbit
+        nuinit (float): True anomaly of the initial orbit in radians (0 or pi)
+        nufinal (float): True anomaly of the final orbit in radians (0 or pi)
+
+    Returns:
+        tuple: (deltava, deltavb, deltavc, dttu)
+            deltava (float): Change in velocity at point A in km/s
+            deltavb (float): Change in velocity at point B in km/s
+            deltavc (float): Change in velocity at point C in km/s
+            dttu (float): Time of flight for the transfer in seconds
+    """
+    # Semi-major axes of initial, transfer, and final orbits
+    ainit = (rinit * (1.0 + einit * np.cos(nuinit))) / (1.0 - einit**2)
+    atran1 = (rinit + rb) * 0.5
+    atran2 = (rb + rfinal) * 0.5
+    afinal = (rfinal * (1.0 + efinal * np.cos(nufinal))) / (1.0 - efinal**2)
+
+    # Initialize outputs
+    deltava, deltavb, deltavc, dttu = 0.0, 0.0, 0.0, 0.0
+
+    # Check if inputs represent elliptical orbits
+    if einit < 1.0 and efinal < 1.0:
+        # Calculate delta-v at point A
+        vinit = np.sqrt((2.0 * const.MU) / rinit - (const.MU / ainit))
+        vtran1a = np.sqrt((2.0 * const.MU) / rinit - (const.MU / atran1))
+        deltava = np.abs(vtran1a - vinit)
+
+        # Calculate delta-v at point B
+        vtran1b = np.sqrt((2.0 * const.MU) / rb - (const.MU / atran1))
+        vtran2b = np.sqrt((2.0 * const.MU) / rb - (const.MU / atran2))
+        deltavb = np.abs(vtran1b - vtran2b)
+
+        # Calculate delta-v at point C
+        vtran2c = np.sqrt((2.0 * const.MU) / rfinal - (const.MU / atran2))
+        vfinal = np.sqrt((2.0 * const.MU) / rfinal - (const.MU / afinal))
+        deltavc = np.abs(vfinal - vtran2c)
+
+        # Calculate total time of flight
+        dttu = np.pi * np.sqrt(atran1**3 / const.MU) + np.pi * np.sqrt(
+            atran2**3 / const.MU
+        )
+
+    return deltava, deltavb, deltavc, dttu
--- software/python/tests/astro/maneuver/test_transfer.py
+++ software/python/tests/astro/maneuver/test_transfer.py
@@ -31,3 +31,33 @@ def test_hohmann(einit, efinal, deltava_expected, deltavb_expected):
     assert custom_isclose(deltava, deltava_expected)
     assert custom_isclose(deltavb, deltavb_expected)
     assert custom_isclose(dttu / const.MIN2SEC, 315.4027569935991)
+
+
+@pytest.mark.parametrize(
+    "einit, efinal, deltava_expected, deltavb_expected, deltavc_expected",
+    [
+        # Vallado 2007, Example 6-2
+        (0.0, 0.0, 3.1562341430432026, 0.6773580299907941, 0.07046593708633986),
+        # Test non-zero eccentricities
+        (0.00123, 0.024, 3.151445140055216, 0.6773580299907941, 0.08278725521142083),
+    ],
+)
+def test_bielliptic(
+    einit, efinal, deltava_expected, deltavb_expected, deltavc_expected
+):
+    # Input values
+    rinit = const.RE + 191.3411
+    rb = const.RE + 503873.0
+    rfinal = const.RE + 376310.0
+    nuinit, nufinal = 0.0, np.pi
+
+    # Calculate bielliptic transfer
+    deltava, deltavb, deltavc, dttu = transfer.bielliptic(
+        rinit, rb, rfinal, einit, efinal, nuinit, nufinal
+    )
+
+    # Expected results
+    assert custom_isclose(deltava, deltava_expected)
+    assert custom_isclose(deltavb, deltavb_expected)
+    assert custom_isclose(deltavc, deltavc_expected)
+    assert custom_isclose(dttu / const.HR2SEC, 593.9199987732167)
