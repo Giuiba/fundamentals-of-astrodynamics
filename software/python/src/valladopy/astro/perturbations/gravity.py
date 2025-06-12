a6a3c30a612cca853dd8245990b712980951b307
Samira Motiwala <23267626+samotiwala@users.noreply.github.com>
2025-06-12 15:18:42 -0700

    Update Gottlieb gravity acceleration function (#137)
    * slight refactor of accel_gott
    
    * clean up
    
    * separate normalization legendre polynomial calcs from gottlieb accel function
    
    * add test for get_norm_gott

--- software/python/src/valladopy/astro/perturbations/gravity.py
+++ software/python/src/valladopy/astro/perturbations/gravity.py
@@ -72,6 +72,55 @@ def read_gravity_field(filename: str, normalized: bool) -> GravityFieldData:
     return gravarr
 
 
+def get_norm_gott(
+    degree: int,
+) -> Tuple[
+    np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray
+]:
+    """Get normalization arrays for the Gottlieb approach.
+
+    References:
+        Vallado: 2022, p. 600, Eq. 8-56
+        Eckman, Brown, Adamo 2016 NASA report
+
+    Args:
+        degree (int): Maximum degree of the gravity field (zonals)
+
+    Returns:
+        tuple: (norm1, norm2, norm11, normn10, norm1m, norm2m, normn1)
+            norm1 (np.ndarray): Normalization Legendre polynomial (1 x degree)
+            norm2 (np.ndarray): Normalization Legendre polynomial (1 x degree)
+            norm11 (np.ndarray): Normalization Legendre polynomial (1 x degree)
+            normn10 (np.ndarray): Normalization Legendre polynomial (1 x degree)
+            norm1m (np.ndarray): Normalization Legendre polynomial (degree x degree)
+            norm2m (np.ndarray): Normalization Legendre polynomial (degree x degree)
+            normn1 (np.ndarray): Normalization Legendre polynomial (degree x degree)
+    """
+    # Normalization arrays
+    size = degree + 1
+    norm1, norm2, norm11, normn10 = (np.zeros(size) for _ in range(4))
+    norm1m, norm2m, normn1 = (np.zeros((size, size)) for _ in range(3))
+
+    for n in range(2, size + 1):
+        norm1[n - 1] = np.sqrt((2 * n + 1) / (2 * n - 1))
+        norm2[n - 1] = np.sqrt((2 * n + 1) / (2 * n - 3))
+        norm11[n - 1] = np.sqrt((2 * n + 1) / (2 * n)) / (2 * n - 1)
+        normn10[n - 1] = np.sqrt((n + 1) * n * 0.5)
+        for m in range(1, n + 1):
+            norm1m[n - 1, m - 1] = np.sqrt(
+                (n - m) * (2 * n + 1) / ((n + m) * (2 * n - 1))
+            )
+            norm2m[n - 1, m - 1] = np.sqrt(
+                (n - m)
+                * (n - m - 1)
+                * (2 * n + 1)
+                / ((n + m) * (n + m - 1) * (2 * n - 3))
+            )
+            normn1[n - 1, m - 1] = np.sqrt((n + m + 1) * (n - m))
+
+    return norm1, norm2, norm11, normn10, norm1m, norm2m, normn1
+
+
 def accel_gott(
     recef: ArrayLike, gravarr: GravityFieldData, degree: int, order: int
 ) -> Tuple[np.ndarray, np.ndarray]:
@@ -88,13 +137,16 @@ def accel_gott(
 
     Returns:
         tuple: (leg_gott_n, accel)
-            leg_gott_n (np.ndarray): Legendre terms
-            accel (np.ndarray): ECEF acceleration vector in km/s^2
+            leg_gott_n (np.ndarray): Legendre terms ([degree + 1 x degree + 1] array)
+            accel (np.ndarray): ECEF acceleration vector in km/s^2 (1 x 3 array)
 
     Notes:
         - This function is able to handle degree and order terms larger than 170 due to
           the formulation.
         - Includes two-body contribution
+
+    TODO:
+        - Add support for partials?
     """
     # Check to make sure gravity field data is normalized
     if not gravarr.normalized:
@@ -109,93 +161,69 @@ def accel_gott(
     muor2 = const.MU * ri**2
 
     # Normalization arrays
-    size = degree + 2
-    norm1, norm2, norm11, normn10 = (np.zeros(size) for _ in range(4))
-    norm1m, norm2m, normn1 = (np.zeros((size, size)) for _ in range(3))
+    norm1, norm2, norm11, normn10, norm1m, norm2m, normn1 = get_norm_gott(degree)
 
-    for n in range(2, size):
-        norm1[n] = np.sqrt((2 * n + 1) / (2 * n - 1))
-        norm2[n] = np.sqrt((2 * n + 1) / (2 * n - 3))
-        norm11[n] = np.sqrt((2 * n + 1) / (2 * n)) / (2 * n - 1)
-        normn10[n] = np.sqrt((n + 1) * n * 0.5)
-        for m in range(1, n + 1):
-            norm1m[n, m] = np.sqrt((n - m) * (2 * n + 1) / ((n + m) * (2 * n - 1)))
-            norm2m[n, m] = np.sqrt(
-                (n - m)
-                * (n - m - 1)
-                * (2 * n + 1)
-                / ((n + m) * (n + m - 1) * (2 * n - 3))
-            )
-            normn1[n, m] = np.sqrt((n + m + 1) * (n - m))
-
-    # Legendre terms
+    # Legendre terms initialization
+    size = degree + 1
     leg_gott_n = np.zeros((size, size))
     leg_gott_n[0, 0] = 1
     leg_gott_n[1, 1] = np.sqrt(3)
     leg_gott_n[1, 0] = np.sqrt(3) * sinlat
 
-    for n in range(2, degree + 1):
-        ni = n
-        leg_gott_n[ni, ni] = norm11[n] * leg_gott_n[n - 1, n - 1] * (2 * n - 1)
+    for n in range(2, size):
+        leg_gott_n[n, n] = norm11[n - 1] * leg_gott_n[n - 1, n - 1] * (2 * n - 1)
 
     ctil, stil = np.zeros(size), np.zeros(size)
     ctil[0], ctil[1] = 1, xor
     stil[1] = yor
 
     sumh, sumgm, sumj, sumk = 0, 1, 0, 0
-    for n in range(2, degree + 1):
-        ni = n
+    for n in range(2, size):
         reorn *= reor
         n2m1 = 2 * n - 1
         nm1 = n - 1
         np1 = n + 1
 
-        # Tesseral (ni, m=ni-1) initial value
-        leg_gott_n[ni, n - 1] = normn1[n, n - 1] * sinlat * leg_gott_n[ni, ni]
+        # Tesseral (n, m=ni-1) initial value
+        leg_gott_n[n, nm1] = normn1[nm1, nm1 - 1] * sinlat * leg_gott_n[n, n]
 
-        # Zonal (ni, m=0)
-        leg_gott_n[ni, 0] = (
-            n2m1 * sinlat * norm1[n] * leg_gott_n[n - 1, 0]
-            - nm1 * norm2[n] * leg_gott_n[n - 2, 0]
+        # Zonal (n, m=0)
+        leg_gott_n[n, 0] = (
+            n2m1 * sinlat * norm1[nm1] * leg_gott_n[nm1, 0]
+            - nm1 * norm2[nm1] * leg_gott_n[nm1 - 1, 0]
         ) / n
 
-        # Tesseral (ni, m=1) initial value
-        leg_gott_n[ni, 1] = (
-            n2m1 * sinlat * norm1m[n, 1] * leg_gott_n[n - 1, 1]
-            - n * norm2m[n, 1] * leg_gott_n[n - 2, 1]
+        # Tesseral (n, m=1) initial value
+        leg_gott_n[n, 1] = (
+            n2m1 * sinlat * norm1m[nm1, 0] * leg_gott_n[nm1, 1]
+            - n * norm2m[nm1, 0] * leg_gott_n[nm1 - 1, 1]
         ) / nm1
 
-        sumhn = normn10[n] * leg_gott_n[ni, 1] * gravarr.c[ni, 0]
-        sumgmn = leg_gott_n[ni, 0] * gravarr.c[ni, 0] * np1
+        sumhn = normn10[nm1] * leg_gott_n[n, 1] * gravarr.c[n, 0]
+        sumgmn = leg_gott_n[n, 0] * gravarr.c[n, 0] * np1
 
         if order > 0:
-            for m in range(2, n - 1):
-                mi = m
-                # Tesseral (n, m)
-                leg_gott_n[ni, mi] = (
-                    n2m1 * sinlat * norm1m[n, m] * leg_gott_n[n - 1, mi]
-                    - (nm1 + m) * norm2m[n, m] * leg_gott_n[n - 2, mi]
+            for m in range(2, nm1):
+                leg_gott_n[n, m] = (
+                    n2m1 * sinlat * norm1m[nm1, m - 1] * leg_gott_n[nm1, m]
+                    - (nm1 + m) * norm2m[nm1, m - 1] * leg_gott_n[nm1 - 1, m]
                 ) / (n - m)
 
             sumjn = sumkn = 0
-            ctil[ni] = ctil[1] * ctil[ni - 1] - stil[1] * stil[ni - 1]
-            stil[ni] = stil[1] * ctil[ni - 1] + ctil[1] * stil[ni - 1]
+            ctil[n] = ctil[1] * ctil[nm1] - stil[1] * stil[nm1]
+            stil[n] = stil[1] * ctil[nm1] + ctil[1] * stil[nm1]
 
             lim = min(n, order)
             for m in range(1, lim + 1):
-                mi = m
-                mp1 = m + 1
-                mxpnm = m * leg_gott_n[ni, mi]
-                bnmtil = gravarr.c[ni, mi] * ctil[mi] + gravarr.s[ni, mi] * stil[mi]
-                sumhn += normn1[n, m] * leg_gott_n[ni, mp1] * bnmtil
-                sumgmn += (n + m + 1) * leg_gott_n[ni, mi] * bnmtil
+                mxpnm = m * leg_gott_n[n, m]
+                bnmtil = gravarr.c[n, m] * ctil[m] + gravarr.s[n, m] * stil[m]
 
-                bnmtm1 = (
-                    gravarr.c[ni, mi] * ctil[mi - 1] + gravarr.s[ni, mi] * stil[mi - 1]
-                )
-                anmtm1 = (
-                    gravarr.c[ni, mi] * stil[mi - 1] - gravarr.s[ni, mi] * ctil[mi - 1]
-                )
+                if m + 1 < leg_gott_n.shape[1]:
+                    sumhn += normn1[nm1, m - 1] * leg_gott_n[n, m + 1] * bnmtil
+                sumgmn += (n + m + 1) * leg_gott_n[n, m] * bnmtil
+
+                bnmtm1 = gravarr.c[n, m] * ctil[m - 1] + gravarr.s[n, m] * stil[m - 1]
+                anmtm1 = gravarr.c[n, m] * stil[m - 1] - gravarr.s[n, m] * ctil[m - 1]
                 sumjn += mxpnm * bnmtm1
                 sumkn -= mxpnm * anmtm1
 
@@ -210,4 +238,4 @@ def accel_gott(
         [lambda_val * xor - sumj, lambda_val * yor - sumk, lambda_val * zor - sumh]
     )
 
-    return leg_gott_n[: degree + 1, : degree + 1], accel
+    return leg_gott_n, accel
